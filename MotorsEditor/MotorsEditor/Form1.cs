498fafbb0a2afa7c1f26fdca9ca638c4e5f60291
alexjshank@gmail.com <alexjshank@gmail.com@0c9e2a0d-1447-0410-93de-f5a27bb8667b>
2008-03-05 15:46:58 +0000

    more editor functionality

--- MotorsEditor/MotorsEditor/Form1.cs
+++ MotorsEditor/MotorsEditor/Form1.cs
@@ -7,7 +7,8 @@ using System.Text;
 using System.Windows.Forms;
 using System.Xml;
 using System.IO;
-using CS
+using System.Threading;
+using CsGL.OpenGL;
 
 namespace MotorsEditor
 {
@@ -17,10 +18,22 @@ namespace MotorsEditor
         bool heightmapEditor_MouseDown = false;
         Graphics heightmapEditor_Graph;
         Pen heightmapEditor_pen;
+        private OpenGLView view;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            view.Refresh();
+        }  
 
         public Form1()
         {
             InitializeComponent();
+
+            view = new OpenGLView();
+            tabControl2.TabPages[1].Controls.Add(view);
+            view.Dock = DockStyle.Fill;
+            timer1.Enabled = true;
+
 
             string currentElement="";
             string value="";
@@ -258,6 +271,188 @@ namespace MotorsEditor
             heightmapEditor_pen = new Pen(heightmapEditor_pen.Color, int.Parse(numericUpDown1.Value.ToString()));
         }
 
-   
+        private void openGLControl1_Click(object sender, EventArgs e)
+        {
+        }
+
+ 
+    }
+
+    class Vector
+    {
+        float x, y, z;
+    }
+
+    class OpenGLView : OpenGLControl
+    {
+        Terrain terrain = new Terrain();
+        float angle = 0;
+        // This function overides the glDraw function found in the CsGL namespace
+        // So that you may customise your viewport settings etc...
+        public override void glDraw()
+        {
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+            GL.glLoadIdentity();
+
+            GL.gluLookAt(terrain.MAP_SIZE / 2, 250, -100, terrain.MAP_SIZE / 2, 0, terrain.MAP_SIZE / 2, 0, 1, 0);
+            GL.glScalef(1, 0.25f, 1);
+            terrain.RenderHeightmap();
+        }
+
+        // This function sets up the OpenGL context as normal, but note the GL prefix's!
+        protected override void InitGLContext()
+        {
+            GL.glShadeModel(GL.GL_SMOOTH);
+            GL.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            GL.glClearDepth(1.0f);
+            GL.glEnable(GL.GL_DEPTH_TEST);
+            GL.glDepthFunc(GL.GL_LEQUAL);
+            GL.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
+
+            terrain.LoadRawFile("c:/height.raw", 512*512);
+        }
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            Size s = Size;
+            double aspect_ratio = (double)s.Width / (double)s.Height;
+            GL.glMatrixMode(GL.GL_PROJECTION); // Select The Projection Matrix
+            GL.glLoadIdentity(); // Reset The Projection Matrix
+            // Calculate The Aspect Ratio Of The Window
+            GL.gluPerspective(45.0f, aspect_ratio, 0.1f, 1000.0f);
+            GL.glMatrixMode(GL.GL_MODELVIEW); // Select The Modelview Matrix
+            GL.glLoadIdentity();// Reset The Modelview Matrix
+        }
+
+  
+    }
+
+    public class Terrain
+    {
+        public int MAP_SIZE = 512;
+        protected int STEP_SIZE = 2;
+        private float HEIGHT_RATIO = 1.5f;
+        private static bool render = true;
+        private byte[] heightmap;
+        private static float scaleValue = 0.15f;
+
+
+        public int Height(int X, int Y)
+        {
+            int x = X % MAP_SIZE;
+            int y = Y % MAP_SIZE;
+
+            if (heightmap == null)
+            {
+                return 0;
+            }
+            int index = x + (y * MAP_SIZE);
+            if (index > 0 && index < MAP_SIZE * MAP_SIZE)
+                return heightmap[index];
+            else return 0;
+        }
+
+        public void LoadRawFile(string filename, int size)
+        {
+
+            FileStream stream = null;
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            BinaryReader reader = null;
+
+            try
+            {
+                // Open The File
+                stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+                reader = new BinaryReader(stream, encoding);
+                MAP_SIZE = (int)Math.Sqrt((double)stream.Length);
+          
+                heightmap = new byte[MAP_SIZE * MAP_SIZE];
+                heightmap = reader.ReadBytes(MAP_SIZE * MAP_SIZE);
+
+            }
+            catch (Exception e)
+            {
+                // Handle Any Exceptions While Loading Terrain Data, Exit App
+                string errorMsg = "An Error Occurred While Loading And Parsing World Data:\n\t" + filename + "\n" + "\n\nStack Trace:\n\t" + e.StackTrace + "\n";
+                MessageBox.Show(errorMsg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
+        }
+
+        public void RenderHeightmap()
+        {
+            int X, Y;
+            int x, y, z;
+
+            if (heightmap == null)
+            {
+                return;
+            }
+
+            if (render)
+            {
+                GL.glBegin(GL.GL_QUADS);
+            }
+            else
+            {
+                GL.glBegin(GL.GL_LINES);
+            }
+
+            for (X = 0; X < MAP_SIZE; X += STEP_SIZE)
+                for (Y = 0; Y < MAP_SIZE; Y += STEP_SIZE)
+                {
+                    // Get The (X, Y, Z) Value For The Bottom Left Vertex
+                    x = X;
+                    y = Height(X, Y);
+                    z = Y;
+                    SetVertexColor(x, z);
+                    GL.glVertex3i(x, y, z);
+
+                    // Get The (X, Y, Z) Value For The Top Left Vertex
+                    x = X;
+                    y = Height(X, Y + STEP_SIZE);
+                    z = Y + STEP_SIZE;
+                    SetVertexColor(x, z);
+                    GL.glVertex3i(x, y, z);
+
+                    // Get The (X, Y, Z) Value For The Top Right Vertex
+                    x = X + STEP_SIZE;
+                    y = Height(X + STEP_SIZE, Y + STEP_SIZE);
+                    z = Y + STEP_SIZE;
+                    SetVertexColor(x, z);
+                    GL.glVertex3i(x, y, z);
+
+                    // Get The (X, Y, Z) Value For The Bottom Right Vertex
+                    x = X + STEP_SIZE;
+                    y = Height(X + STEP_SIZE, Y);
+                    z = Y;
+                    SetVertexColor(x, z);
+                    GL.glVertex3i(x, y, z);
+                }
+            GL.glEnd();
+            GL.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+
+        private void SetVertexColor(int x, int y)
+        {
+            if (heightmap == null)
+            {
+                return;
+            }
+
+            float fColor = -0.15f + (Height(x, y) / 256.0f);
+
+            GL.glColor3f(0.0f, 0.0f, fColor);
+        }
     }
 }
